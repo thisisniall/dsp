9b2b422ce7ec95e2c9c2486d94cdd3c3d00fd548
Niall Murphy <niallpatrickmurphy@gmail.com>
2016-08-31 22:05:09 -0400

    strings and list excercises completed. still need to complete questions on the basic python page.

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -37,8 +37,9 @@ def both_ends(s):
     if len(s) < 2:
         return ''
     else:
-        # blegh, review string slicing
-        return s[:2],s[:2:-1]    
+        first_2 = s[:2]
+        last_2 = s[-2:]
+        return first_2 + last_2 
 
     """
     Given a string s, return a string made of the first 2 and the last
@@ -57,14 +58,21 @@ def both_ends(s):
     """
     raise NotImplementedError
 #testing
-print both_ends('spring')
-print both_ends('Hello')
-print both_ends('a')
-print both_ends('xyz')
-
-
+# print both_ends('spring')
+# print both_ends('Hello')
+# print both_ends('a')
+# print both_ends('xyz')
+# works
 
 def fix_start(s):
+    first_char = s[0]
+    lst = list(s)
+    for i in range(1,len(lst)):
+        if lst[i] == first_char:
+            lst[i] = "*"
+    new_word = "".join(lst)
+    return new_word
+
     """
     Given a string s, return a string where all occurences of its
     first char have been changed to '*', except do not change the
@@ -81,9 +89,25 @@ def fix_start(s):
     'donut'
     """
     raise NotImplementedError
+#testing 
+# print fix_start('babble')
+# print fix_start('aardvark')
+# print fix_start('google')
+# print fix_start('donut')
+# works
 
 
 def mix_up(a, b):
+    first_2_a = a[:2]
+    first_2_b = b[:2]
+    after_2_a = a[2:]
+    after_2_b = b[2:]
+    new_a = first_2_b+after_2_a
+    new_b = first_2_a+after_2_b
+    return new_a+" "+new_b
+
+# this could definitely be more elegant, to the point of a one-line solution
+
     """
     Given strings a and b, return a single string with a and b
     separated by a space '<a> <b>', except swap the first 2 chars of
@@ -100,8 +124,22 @@ def mix_up(a, b):
     """
     raise NotImplementedError
 
+#testing
+# print mix_up('mix', 'pod')
+# print mix_up('dog', 'dinner')
+# print mix_up('gnash', 'sport')
+# print mix_up('pezzy', 'firm')
+# works
 
 def verbing(s):
+    # ing case
+    if len(s) >= 3 and s[-3:] == "ing":
+        s = s + "ly"
+    elif len(s) >= 3:
+        s = s+ "ing"
+    # no need to create else case, since we are leaving it unchanged and doing nothing else    
+    return s
+
     """
     Given a string, if its length is at least 3, add 'ing' to its end.
     Unless it already ends in 'ing', in which case add 'ly' instead.
@@ -117,8 +155,49 @@ def verbing(s):
     """
     raise NotImplementedError
 
+#tests
+# print verbing('hail')
+# print verbing('swiming')
+# print verbing('do')
+# works
 
-def not_bad(s):
+# -----------
+
+# old version, failed when punctuation was used. need regex to solve the general case which includes punctuation, but regex is not taught in thinkpython or codeacademy.
+
+# def not_bad(s):
+#     # splits into list of words
+#     lst = s.split()
+#     not_marker = False
+#     bad_marker = False
+#     for i in range(0, len(lst)):
+#         if lst[i] == "not":
+#             not_marker = i
+#         elif lst[i] == "bad":
+#             bad_marker = i
+#     if not_marker and bad_marker and not_marker < bad_marker:
+#         new_lst = []
+#         # modify things
+#         for i in range(0, len(lst)):
+#             if i == not_marker:
+#                 new_lst.append("good")
+#             elif i > not_marker and i <= bad_marker:
+#                 continue
+#             else:
+#                 new_lst.append(lst[i])
+#         return " ".join(new_lst)
+#     else:
+#         return s
+# edit: yeah, I think we need regex here to cover the "bad!" case.
+
+import re
+
+def not_bad(string):
+    return re.sub(r'\bnot\b.*\bbad\b' ,'good', string)
+    # note - this version is case sensitive, but so was my original
+
+# from dave: (And r'asggs' = raw string, very useful when using regular expressions to avoid a forest of backslashes.) (\b = word boundary)
+
     """
     Given a string, find the first appearance of the substring 'not'
     and 'bad'. If the 'bad' follows the 'not', replace the whole
@@ -137,8 +216,28 @@ def not_bad(s):
     """
     raise NotImplementedError
 
+#tests
+# print "one case of not_bad (with a !) still non-functional, non-regex implementation?"
+print not_bad('This movie is not so bad')
+print not_bad('This dinner is not that bad!')
+print not_bad('This tea is not hot')
+print not_bad("It's bad yet not")
+# text cases pass with regex
+
 
 def front_back(a, b):
+    a_back = a[-(len(a)/2):]
+    b_back = b[-(len(b)/2):]
+    if len(a) % 2 == 0:
+        a_front = a[:(len(a)/2)]
+    else:
+        a_front = a[:int((len(a)/2)+1)]
+    if len(b) % 2 == 0:
+        b_front = b[:(len(b)/2)]
+    else:
+        b_front = b[:int((len(b)/2)+1)]  
+    return a_front + b_front + a_back + b_back
+
     """
     Consider dividing a string into two halves. If the length is even,
     the front and back halves are the same length. If the length is
@@ -155,3 +254,8 @@ def front_back(a, b):
     'KitDontenut'
     """
     raise NotImplementedError
+#tests
+# print front_back('abcd', 'xy') 
+# print front_back('abcde', 'xyz')
+# print front_back('Kitten', 'Donut')
+#all pass
--- python/q7_lists.py
+++ python/q7_lists.py
@@ -3,6 +3,12 @@
 
 
 def match_ends(words):
+    count = 0
+    for i in range(0,len(words)):
+        if len(words[i]) >= 2 and words[i][0] == words[i][-1:]:
+            count +=1
+    return count
+
     """
     Given a list of strings, return the count of the number of strings
     where the string length is 2 or more and the first and last chars
@@ -17,8 +23,28 @@ def match_ends(words):
     """
     raise NotImplementedError
 
+#tests
+# print match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
+# print match_ends(['', 'x', 'xy', 'xyx', 'xx'])
+# print match_ends(['aaa', 'be', 'abc', 'hello'])
+#all work
 
 def front_x(words):
+    #psuedocode:
+    # break words into 2 lists, one with strings that begin with x, one without
+    # sort both lists
+    # combine lists
+    x_start = []
+    non_x_start = []
+    for word in words:
+        if word[0].lower() == "x": #.lower() for case sensitivity
+            x_start.append(word)
+        else:
+            non_x_start.append(word)
+    x_start.sort()
+    non_x_start.sort()
+    return x_start + non_x_start
+
     """
     Given a list of strings, return a list with the strings in sorted
     order, except group all the strings that begin with 'x' first.
@@ -34,6 +60,11 @@ def front_x(words):
     """
     raise NotImplementedError
 
+#tests
+# print front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
+# print front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
+# print front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
+# all tests pass
 
 def sort_last(tuples):
     """
@@ -49,8 +80,21 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
 
+    # x = sorted(tuples,key= lambda x: x[1])
+    # note that this technically works for the above examples, but doesn't work in the general case, i have added an additional 4th example that this fails for
+    x = sorted(tuples,key= lambda x: x[len(x)-1])
+    return x
+
+
+    raise NotImplementedError
+# #test cases
+# print sort_last([(1, 3), (3, 2), (2, 1)])
+# print sort_last([(2, 3), (1, 2), (3, 1)])
+# print sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
+# # extra test case
+# print sort_last([(1, 7), (1, 3), (1, 8, 5), (2, 6, 12)])
+# #test cases clear
 
 def remove_adjacent(nums):
     """
@@ -68,8 +112,18 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
+    # trick is to embed while within for loop to ensure it runs as many times as necessary rather than just using an if statement
+    for i in nums:
+        while i < len(nums)-1 and nums[i] == nums[i+1]:
+            del nums[i]
+    return nums
 
+    raise NotImplementedError
+# tests 
+# print remove_adjacent([1, 2, 2, 3])
+# print remove_adjacent([2, 2, 3, 3, 3])
+# print remove_adjacent([3, 2, 3, 3, 3])
+# all tests pass
 
 def linear_merge(list1, list2):
     """
@@ -85,4 +139,11 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
+    # am i missing something here? this seems childishly simple compared to previous problems. or does the built-in sorted algorithm not work in linear time?
+    return sorted(list1+list2)
+
     raise NotImplementedError
+
+print linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
+print linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
+print linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
